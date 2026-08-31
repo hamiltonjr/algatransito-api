117abd4deb10694b402068b45cf64b3ad010a02d
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2026-08-31 15:55:30 -0300

    Inclui implementação de queries que tornam nossa aplicação capaz de persistir dados.

--- src/main/java/com/algaworks/algatransito_api/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito_api/api/controller/ProprietarioController.java
@@ -1,5 +1,8 @@
 package com.algaworks.algatransito_api.api.controller;
 import com.algaworks.algatransito_api.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
@@ -7,20 +10,12 @@ import java.util.List;
 
 @RestController
 public class ProprietarioController {
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("João");
-        proprietario1.setTelefone("34 99999-1111");
-        proprietario1.setEmail("joaodascouves@algaworks.com");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Maria");
-        proprietario2.setTelefone("11 97777-1111");
-        proprietario2.setEmail("mariadasilva@algaworks.com");
-
-        return Arrays.asList(proprietario1, proprietario2);
+        return manager.createQuery("from Proprietario", Proprietario.class)
+                .getResultList();
     }
 }
