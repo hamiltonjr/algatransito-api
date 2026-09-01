27de3bd7bf44ccd92c753791d77b0ccb94bcd806
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2026-09-01 06:19:31 -0300

    Inclui injeção de repositório via construtor (Lombok).

--- src/main/java/com/algaworks/algatransito_api/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito_api/api/controller/ProprietarioController.java
@@ -1,21 +1,18 @@
 package com.algaworks.algatransito_api.api.controller;
 import com.algaworks.algatransito_api.domain.model.Proprietario;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
+import com.algaworks.algatransito_api.domain.repository.ProprietarioRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class ProprietarioController {
-    @PersistenceContext
-    private EntityManager manager;
+    private final ProprietarioRepository proprietarioRepository;;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        return manager.createQuery("from Proprietario", Proprietario.class)
-                .getResultList();
+        return proprietarioRepository.findAll();
     }
 }
