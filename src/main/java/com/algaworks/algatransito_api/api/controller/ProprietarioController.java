ac5100c984b2152718156a90be136467d8332a67
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2026-09-02 06:36:50 -0300

    Inclui busca de proprietário (registro individual por id).

--- src/main/java/com/algaworks/algatransito_api/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito_api/api/controller/ProprietarioController.java
@@ -2,17 +2,28 @@ package com.algaworks.algatransito_api.api.controller;
 import com.algaworks.algatransito_api.domain.model.Proprietario;
 import com.algaworks.algatransito_api.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
     private final ProprietarioRepository proprietarioRepository;;
 
-    @GetMapping("/proprietarios")
+    @GetMapping
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
